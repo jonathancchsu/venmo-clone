cb35f346d53276abf1e9d24e4a8547939a5b7c25
Jonathan H <jhsucoding@gmail.com>
2022-04-12 15:46:51 -0400

    working on models

--- app/models/requests.py
+++ app/models/requests.py
@@ -11,4 +11,16 @@ class Requests(db.Model):
   __tablename__ = 'requests'
 
   id = db.Column(Integer, primary_key=True)
-  
+  sender_id = db.Column(Integer, ForeignKey('user.id'), nullable=False)
+  receiver_id = db.Column(Integer, ForeignKey('user.id'), nullable=False)
+  created_at = db.Column(DateTime(timezone=True), server_default=func.now())
+  updated_at = db.Column(DateTime(timezone=True), onupdate=func.now())
+
+  def to_dict(self):
+    return {
+      'id': self.id,
+      'sender_id': self.sender_id,
+      'receiver_id': self.receiver_id,
+      'created_at': self.created_at,
+      'updated_at': self.updated_at,
+    }
