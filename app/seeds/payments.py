9c687eabc95ce701c8ef9f8aa8f20bef1741e591
Jonathan H <jhsucoding@gmail.com>
2022-04-13 08:27:04 -0400

    finalized payments data

--- app/seeds/payments.py
+++ app/seeds/payments.py
@@ -1,16 +1,18 @@
 from app.models import db, Payment
 
 def seed_payments():
-  payments = [{'sender_id': 1, 'receiver_id': 2, 'title': '🍜', 'amount': 9.50},
-              {'sender_id': 1, 'receiver_id': 3, 'title': '🍣', 'amount': 10.00},
-              {'sender_id': 2, 'receiver_id': 1, 'title': '🍰', 'amount': 5.95},
-              {'sender_id': 2, 'receiver_id': 3, 'title': '🍾', 'amount': 200.00},
-              {'sender_id': 3, 'receiver_id': 1, 'title': '🧋', 'amount': 6.95},
-              {'sender_id': 3, 'receiver_id': 2, 'title': '⌨️', 'amount': 500.00},]
+  payments = [
+                {'sender_id': 1, 'receiver_id': 2, 'title': '🍜', 'amount': 9.50, 'privacy': 'public',},
+                {'sender_id': 1, 'receiver_id': 3, 'title': '🍣', 'amount': 10.00, 'privacy': 'public',},
+                {'sender_id': 2, 'receiver_id': 1, 'title': '🍰', 'amount': 5.95, 'privacy': 'public',},
+                {'sender_id': 2, 'receiver_id': 3, 'title': '🍾', 'amount': 200.00, 'privacy': 'public',},
+                {'sender_id': 3, 'receiver_id': 1, 'title': '🧋', 'amount': 6.95, 'privacy': 'public',},
+                {'sender_id': 3, 'receiver_id': 2, 'title': '⌨️', 'amount': 500.00, 'privacy': 'public',},
+              ]
 
   for payment in payments:
     new_payment = Payment(
-      sender_id = payment['sender_id'], receiver_id = payment['receiver_id'], title = payment['title'], amount = payment['amount'],
+      sender_id = payment['sender_id'], receiver_id = payment['receiver_id'], title = payment['title'], amount = payment['amount'], privacy = payment['privacy'],
     )
     db.session.add(new_payment)
     db.session.commit()
