0d8828c1658f11c0270528446b1edd0ee108659b
Jonathan H <jhsucoding@gmail.com>
2022-04-13 08:30:08 -0400

    finalized requests data

--- app/seeds/requests.py
+++ app/seeds/requests.py
@@ -1,16 +1,18 @@
 from app.models import db, Request
 
 def seed_requests():
-  requests = [{'sender_id': 1, 'receiver_id': 2, 'title': '🍜', 'amount': 9.50},
-              {'sender_id': 1, 'receiver_id': 3, 'title': '🍣', 'amount': 10.00},
-              {'sender_id': 2, 'receiver_id': 1, 'title': '🍰', 'amount': 5.95},
-              {'sender_id': 2, 'receiver_id': 3, 'title': '🍾', 'amount': 200.00},
-              {'sender_id': 3, 'receiver_id': 1, 'title': '🧋', 'amount': 6.95},
-              {'sender_id': 3, 'receiver_id': 2, 'title': '⌨️', 'amount': 500.00},]
+  requests = [
+                {'sender_id': 1, 'receiver_id': 2, 'title': '🍜', 'amount': 9.50, 'privacy': 'public', 'completed': False,},
+                {'sender_id': 1, 'receiver_id': 3, 'title': '🍣', 'amount': 10.00, 'privacy': 'public', 'completed': False,},
+                {'sender_id': 2, 'receiver_id': 1, 'title': '🍰', 'amount': 5.95, 'privacy': 'public', 'completed': False,},
+                {'sender_id': 2, 'receiver_id': 3, 'title': '🍾', 'amount': 200.00, 'privacy': 'public', 'completed': False,},
+                {'sender_id': 3, 'receiver_id': 1, 'title': '🧋', 'amount': 6.95, 'privacy': 'public', 'completed': False,},
+                {'sender_id': 3, 'receiver_id': 2, 'title': '⌨️', 'amount': 500.00, 'privacy': 'public', 'completed': False,},
+              ]
 
   for request in requests:
     new_request = request(
-      sender_id = request['sender_id'], receiver_id = request['receiver_id'], title = request['title'], amount = request['amount'],
+      sender_id = request['sender_id'], receiver_id = request['receiver_id'], title = request['title'], amount = request['amount'], privacy = request['privacy'], completed = request['completed'],
     )
     db.session.add(new_request)
     db.session.commit()
