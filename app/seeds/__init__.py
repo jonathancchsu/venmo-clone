88a08fb6a323b8be626962b70b491aabda864104
Jonathan H <jhsucoding@gmail.com>
2022-04-13 09:07:00 -0400

    database initpy

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,10 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .comments import seed_comments, undo_comments
+from .likes import seed_likes, undo_likes
+from .payments import seed_payments, undo_payments
+from .requests import seed_requests, undo_requests
+from .socials import seed_socials, undo_socials
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -10,6 +15,11 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     seed_users()
+    seed_comments()
+    seed_likes()
+    seed_payments()
+    seed_requests()
+    seed_socials()
     # Add other seed functions here
 
 
@@ -17,4 +27,9 @@ def seed():
 @seed_commands.command('undo')
 def undo():
     undo_users()
+    undo_comments()
+    undo_likes()
+    undo_payments()
+    undo_requests()
+    undo_socials()
     # Add other undo functions here
