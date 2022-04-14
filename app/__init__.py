1819c2acdfbba824d548e17a1ed7552ee15e68d4
Jonathan H <jhsucoding@gmail.com>
2022-04-14 06:21:18 -0400

    backend init

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from flask_login import LoginManager
 from .models import db, User, Comment, Like, Payment, Request, Social
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
+from .api import comments, likes, payments, requests, socials
 
 from .seeds import seed_commands
 
@@ -31,6 +32,11 @@ app.cli.add_command(seed_commands)
 app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
+app.register_blueprint(comments.bp, url_prefix='/api/comments')
+app.register_blueprint(likes.bp, url_prefix='/api/likes')
+app.register_blueprint(payments.bp, url_prefix='/api/payments')
+app.register_blueprint(requests.bp, url_prefix='/api/requests')
+app.register_blueprint(socials.bp, url_prefix='/api/socials')
 db.init_app(app)
 Migrate(app, db)
 
