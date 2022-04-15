b576c09b2fac86fb631fdf8022cc2a974c2f8edc
Jonathan H <jhsucoding@gmail.com>
2022-04-15 16:22:10 -0400

    working

---
+++ react-app/src/components/MainVIew/MainContent/Home/index.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react'
+import { useSelector, useDispatch} from 'react-redux'
+import { Link } from 'react-router-dom'
+import * as sessionActions from '../../../../store/session'
+
+import { getAllPayments } from '../../../../store/payment'
+
+import './Home.css'
+
+function Home () {
+  const dispatch = useDispatch()
+  const sessionUser = useSelector(state => state.sesion.user)
+  const allPayments = useSelector(state => state.payments.entries)
+
+  useEffect(() => {
+    dispatch(getAllPayments())
+    dispatch(sessionActions.restore())
+  }, [dispatch])
+
+  return (
+    <div>
+      {}
+    </div>
+  )
+}
